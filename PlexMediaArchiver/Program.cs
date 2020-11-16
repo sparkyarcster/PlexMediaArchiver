6fea04c17c8d119e79b43c472b7823ab324dc15d
Stephon Parker <sgparker62@gmail.com>
2020-11-15 20:08:38 -0500

    adding location path and remove app config from tracking

--- PlexMediaArchiver/Program.cs
+++ PlexMediaArchiver/Program.cs
@@ -11,20 +11,20 @@ namespace PlexMediaArchiver
 
             //Stil unclear to me at this point if this data includes ALL home users, or simply the main Plex account.
 
-            //var moviesNotViewed = api.GetMoviesNotViewed();
+            var moviesNotViewed = api.GetMoviesNotViewed();
 
-            //foreach(var movie in moviesNotViewed)
-            //{
-            //    var details = $"{movie.MetaData.Title} -> {(movie.LastViewed.HasValue ? movie.LastViewed.ToString() : "Never")}";
-            //    Console.WriteLine(details);
-            //}
-
-            var movies = api.GetMoviesByTitle("Indiana");
-
-            foreach (var movie in movies)
+            foreach(var movie in moviesNotViewed)
             {
                 var details = $"{movie.MetaData.Title} -> {(movie.LastViewed.HasValue ? movie.LastViewed.ToString() : "Never")}";
                 Console.WriteLine(details);
+            }
+
+            //var movies = api.GetMoviesByTitle("Indiana");
+
+            //foreach (var movie in movies)
+            //{
+              //  var details = $"{movie.MetaData.Title} -> {(movie.LastViewed.HasValue ? movie.LastViewed.ToString() : "Never")}";
+                //Console.WriteLine(details);
             }
         }
     }
